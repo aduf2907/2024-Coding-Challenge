5f1a86f98d3f05bea235801fb9d378f16491d49f
aduf <dufalmira@gmail.com>
2024-01-24 22:54:39 +0700

    day 2 - reverseSeq

---
+++ 2-reservedseq.js
@@ -0,0 +1,12 @@
+// link: https://www.codewars.com/kata/5a00e05cc374cb34d100000d
+
+const reverseSeq = (n) => {
+  let array = [];
+
+  for (let i = n; i > 0; i--) {
+    array.push(i);
+  }
+  return array;
+};
+
+console.log(reverseSeq(5));
