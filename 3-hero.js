83e0bffeec2fee1842931804d35b4441a9b3b6da
aduf <dufalmira@gmail.com>
2024-01-25 23:47:49 +0700

    challenge day 3-hero

---
+++ 3-hero.js
@@ -0,0 +1,17 @@
+// link: https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
+
+// function hero(bullets, dragons) {
+//   //Get Coding!
+
+//   if (bullets / 2 >= dragons) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+function hero(bullets, dragons) {
+  return bullets / 2 >= dragons;
+}
+
+console.log(hero(10, 5));
