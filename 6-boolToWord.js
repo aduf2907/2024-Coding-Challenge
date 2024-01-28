6a3de1436ce77a792c1f14b21ed6fde61bf92440
aduf <dufalmira@gmail.com>
2024-01-28 14:52:30 +0700

    challenge day 6 - boolToWord

---
+++ 6-boolToWord.js
@@ -0,0 +1,8 @@
+//link: https://www.codewars.com/kata/53369039d7ab3ac506000467
+
+function boolToWord(bool) {
+  // return bool == true ? "Yes" : "No";
+  return bool ? "Yes" : "No";
+}
+
+console.log(boolToWord(true));
