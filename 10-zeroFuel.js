41f45b1ac6d2b45d9fd1ee764ac5cf9132962ca6
aduf <dufalmira@gmail.com>
2024-01-29 09:34:37 +0700

    challenge day 7 - zeroFuel

---
+++ 10-zeroFuel.js
@@ -0,0 +1,8 @@
+//link: https://www.codewars.com/kata/5861d28f124b35723e00005e
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  // TODO
+  return distanceToPump / mpg <= fuelLeft;
+};
+
+console.log(zeroFuel(50, 25, 2));
