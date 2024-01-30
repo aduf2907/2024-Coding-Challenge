20a4f66398b88585ccc77cf2c88d5309235d1c35
aduf <dufalmira@gmail.com>
2024-01-30 17:02:06 +0700

    menambahkan link soal

--- 13-DNAtoRNA.js
+++ 13-DNAtoRNA.js
@@ -1,3 +1,5 @@
+//link: https://www.codewars.com/kata/5556282156230d0e5e000089/javascript
+
 // Function Declaration
 // function DNAtoRNA(dna) {
 //   // create a function which returns an RNA sequence from the given DNA sequence
