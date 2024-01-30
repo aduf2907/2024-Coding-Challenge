e612e48927ce22cde520e30d9704f6cbf7a589db
aduf <dufalmira@gmail.com>
2024-01-30 17:00:40 +0700

    menambahkan cara lainnya

--- 13-DNAtoRNA.js
+++ 13-DNAtoRNA.js
@@ -1,6 +1,13 @@
-function DNAtoRNA(dna) {
-  // create a function which returns an RNA sequence from the given DNA sequence
-  return dna.replace(/T/g, "U");
-}
+// Function Declaration
+// function DNAtoRNA(dna) {
+//   // create a function which returns an RNA sequence from the given DNA sequence
+//   return dna.replace(/T/g, "U");
+// }
+
+// Function Ekspression
+// const DNAtoRNA = (dna) => dna.replaceAll("T", "U");
+
+const DNAtoRNA = (dna) => dna.split("T").join("U");
 
 console.log(DNAtoRNA("GCAT"));
+console.log(DNAtoRNA("TTTT"));
