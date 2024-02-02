7b30e3165720ad417b5f0a9db78f5edee3430e51
aduf <dufalmira@gmail.com>
2024-02-02 18:04:19 +0700

    challenge day 11-countSheep

---
+++ 18-countSheep.js
@@ -0,0 +1,15 @@
+//link: https://www.codewars.com/kata/5b077ebdaf15be5c7f000077
+
+// var countSheep = function (num) {
+//   //your code here
+//   let result = "";
+//   for (let i = 1; i <= num; i++) {
+//     result += i + " sheep...";
+//   }
+//   return result;
+// };
+
+const countSheep = (num) =>
+  [...Array(num)].map((_, i) => `${i + 1} sheep...`).join("");
+
+console.log(countSheep(3));
