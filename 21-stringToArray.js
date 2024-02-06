9d7afc362912345e230af055777b5c768eeeb083
aduf <dufalmira@gmail.com>
2024-02-06 21:50:32 +0700

    challenge day 13 - stringToArray

---
+++ 21-stringToArray.js
@@ -0,0 +1,7 @@
+//link: https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+
+function stringToArray(string) {
+  return string.split(" ");
+}
+
+console.log(stringToArray("I love arrays they are my favorite"));
