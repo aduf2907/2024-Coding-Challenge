b72b10cd5236ebc7ecaa2a867b3f4245ff85c0b0
aduf <dufalmira@gmail.com>
2024-02-18 15:21:51 +0700

    next challenge day 19 - opposite

---
+++ 30-opposite.js
@@ -0,0 +1,8 @@
+//link: https://www.codewars.com/kata/56dec885c54a926dcd001095/train/javascript
+
+function opposite(number) {
+  return number === -number ? number : -number;
+}
+
+console.log(opposite(23));
+console.log(opposite(-12525220.3325));
