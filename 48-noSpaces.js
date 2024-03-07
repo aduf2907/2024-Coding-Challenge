dd048ed5dd83228c244cace21fd5549c5ecf3662
aduf <dufalmira@gmail.com>
2024-03-07 12:50:54 +0700

    next challenge day 31 - noSpace

---
+++ 48-noSpaces.js
@@ -0,0 +1,7 @@
+//link: https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+
+function noSpace(x) {
+  return x.split(" ").join("");
+}
+
+console.log(noSpace("8 j 8   mBliB8g  imjB8B8  jl  B"));
