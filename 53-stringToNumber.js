dcac9bc8932204973a97b8845d953a6ec40dac94
aduf <dufalmira@gmail.com>
2024-03-12 17:47:56 +0700

    next challenge day 34 - stringToNumber

---
+++ 53-stringToNumber.js
@@ -0,0 +1,8 @@
+//link: https://www.codewars.com/kata/544675c6f971f7399a000e79
+// ch: 34
+
+const stringToNumber = function (str) {
+  return parseInt(str);
+};
+
+console.log(stringToNumber("123"));
