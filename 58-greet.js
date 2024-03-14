04867d6ac25055c71602ea79a043ce6e63989c19
aduf <dufalmira@gmail.com>
2024-03-14 17:45:59 +0700

    challenge day 36 - greet

---
+++ 58-greet.js
@@ -0,0 +1,8 @@
+//link: https://www.codewars.com/kata/55a70521798b14d4750000a4/train/javascript
+//ch: 36
+
+function greet(name) {
+  return `Hello, ${name} how are you doing today?`;
+}
+
+console.log(greet("Adup"));
