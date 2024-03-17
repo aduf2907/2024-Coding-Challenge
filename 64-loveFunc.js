f9c6dcb2034c9a0b72ab8feff2e67dae02fc660e
aduf <dufalmira@gmail.com>
2024-03-17 22:57:47 +0700

    next challenge day 38 - loveFunc

---
+++ 64-loveFunc.js
@@ -0,0 +1,9 @@
+//link: https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+//ch: 38
+
+function lovefunc(flower1, flower2) {
+  return (flower1 + flower2) % 2 === 1;
+}
+
+console.log(lovefunc(2, 3));
+console.log(lovefunc(3, 3));
