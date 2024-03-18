6848c0da694e8f0471e821946791082eda20da94
aduf <dufalmira@gmail.com>
2024-03-18 14:29:18 +0700

    next challenge day 39 - addBinary

---
+++ 66-addBinary.js
@@ -0,0 +1,9 @@
+//link: https://www.codewars.com/kata/551f37452ff852b7bd000139/javascript
+//ch: 39
+
+function addBinary(a, b) {
+  return (a + b).toString(2);
+}
+
+console.log(addBinary(1, 1));
+console.log(addBinary(5, 9));
