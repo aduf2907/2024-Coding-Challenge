69a9bbe6494d4bcc9069392281acfcace50e59ed
aduf <dufalmira@gmail.com>
2024-10-27 23:26:07 +0700

    day 56 - otherAngle

---
+++ 92-otherAngle.js
@@ -0,0 +1,8 @@
+// link: https://www.codewars.com/kata/5a023c426975981341000014/train/javascript
+// day 56
+
+function otherAngle(a, b) {
+  return 180 - (a + b);
+}
+
+console.log(otherAngle(60, 60));
